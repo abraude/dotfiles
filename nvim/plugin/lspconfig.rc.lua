4acc278dd17c44154e32634dc39fa16063899a54
Chuk <chuk@loki.local>
2022-09-18 21:28:40 +0200

    Add packer_plugin global variable to the list

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -67,7 +67,7 @@ nvim_lsp.sumneko_lua.setup {
     Lua = {
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { 'vim', 'packer_plugins' },
       },
 
       workspace = {
