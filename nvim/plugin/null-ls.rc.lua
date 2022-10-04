99ba4f5423df59774d3d5292736cc07ad328932f
Chuk <chuk@loki.local>
2022-10-04 13:37:13 +0200

    Ignor .git directory

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -14,20 +14,19 @@ local sources = {
   formatting.lua_format,
 }
 
-local on_attach = function(client)
-  if client.resolved_capabilities.document_formatting then
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
   end
-
-  vim.cmd [[
-    augroup document_highlight
-      autocmd! * <buffer>
-      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-  ]]
 end
-
 
 null_ls.setup({
   sources = sources,
