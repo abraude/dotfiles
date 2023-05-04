034fafd3ce8525d30f7145b05a95fb07441d2e05
Chuk <chuk@loki.local>
2023-05-04 16:45:33 +0200

    New plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,21 +12,21 @@ packer.startup(function(use)
     requires = { "tjdevries/colorbuddy.nvim" }
   }
 
-  use "nvim-lua/plenary.nvim" -- Common utilities
+  use "nvim-lua/plenary.nvim"        -- Common utilities
   use "kyazdani42/nvim-web-devicons" -- File icons
-  use "goolord/alpha-nvim" -- Startup page
+  use "goolord/alpha-nvim"           -- Startup page
 
   use "L3MON4D3/LuaSnip"
 
-  use "nvim-lualine/lualine.nvim" -- Statusline
-  use "onsails/lspkind-nvim" -- vscode-like pictograms
-  use "hrsh7th/cmp-buffer" -- nvim-cmp source for buffer words
-  use "hrsh7th/cmp-nvim-lsp" -- nvim-cmp source for neovim's built-in LSP
-  use "hrsh7th/nvim-cmp" -- Completion
-  use "neovim/nvim-lspconfig" -- LSP
-  use "glepnir/lspsaga.nvim" -- LSP UIs
+  use "nvim-lualine/lualine.nvim"       -- Statusline
+  use "onsails/lspkind-nvim"            -- vscode-like pictograms
+  use "hrsh7th/cmp-buffer"              -- nvim-cmp source for buffer words
+  use "hrsh7th/cmp-nvim-lsp"            -- nvim-cmp source for neovim's built-in LSP
+  use "hrsh7th/nvim-cmp"                -- Completion
+  use "neovim/nvim-lspconfig"           -- LSP
+  use "glepnir/lspsaga.nvim"            -- LSP UIs
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+  use 'MunifTanjim/prettier.nvim'       -- Prettier plugin for Neovim's built-in LSP client
 
   use "windwp/nvim-autopairs"
   use "windwp/nvim-ts-autotag"
@@ -46,7 +46,7 @@ packer.startup(function(use)
   use "norcalli/nvim-colorizer.lua"
 
   use "lewis6991/gitsigns.nvim"
-  use "dinhhuy258/git.nvim" -- For git blame & browse
+  use "dinhhuy258/git.nvim"                 -- For git blame & browse
 
   use "lukas-reineke/indent-blankline.nvim" -- IndentLine event = "BufReadPre",
   use "p00f/nvim-ts-rainbow"
@@ -69,6 +69,19 @@ packer.startup(function(use)
     run = function() vim.fn["mkdp#util#install"]() end,
   })
 
+  -- use "nvim-tree/nvim-tree.lua"
+  -- Unless you are still migrating, remove the deprecated commands from v1.x
+  vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+
   -- use {
   --   "github/copilot.vim",
   --   branch = "release",
@@ -79,5 +92,4 @@ packer.startup(function(use)
   --     end
   --   end
   -- }
-
 end)
